6ced8640fe5006d6ca432169ed2fdf60b5a8ed3c
ssLC <chenletaoop@gmail.com>
2017-05-06 19:24:39 -0700

    richard, stop pushing test folders

--- test/lyrics.js
+++
@@ -1,6 +0,0 @@
-const api_key = process.env.GENIUS_KEY;
-var lyricist = require('lyricist')(api_key);
-
-lyricist.song({search: "Kanye West Famous"}, function (err, song) {
-  console.log("%s", song.lyrics);
-});
